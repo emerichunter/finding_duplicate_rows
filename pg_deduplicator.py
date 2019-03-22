10a80145de671960efe4d5a90437fc17ae7759c9
emerichunter <areyoudeadyet667@yahoo.fr>
2019-03-22 15:18:52 +0100

    Added DB error handling, rollback and custom data types/domains
    - TODO

--- pg_deduplicator.py
+++ pg_deduplicator.py
@@ -7,27 +7,28 @@ import psycopg2
 # Try to connect
 
 try:
-    conn=psycopg2.connect("dbname='postgres' user='postgres'")
+    conn=psycopg2.connect("dbname='sagaie' user='postgres'")
 except:
     print "I am unable to connect to the database."
 
 cur = conn.cursor()
 try:
     cur.execute("""WITH pks AS (
-SELECT tc.table_schema, tc.table_name, kc.column_name
-FROM information_schema.table_constraints tc
-JOIN information_schema.key_column_usage kc ON kc.table_name = tc.table_name
-AND kc.table_schema = tc.table_schema AND kc.constraint_name = tc.constraint_name
+SELECT tc.table_schema, tc.table_name, kc.position_in_unique_constraint, array_to_string(array_agg(kc.column_name::text), ', ') as column_name
+FROM information_schema.table_constraints tc 
+JOIN information_schema.key_column_usage kc ON kc.table_name = tc.table_name 
+AND kc.table_schema = tc.table_schema AND kc.constraint_name = tc.constraint_name 
 WHERE (tc.constraint_type = 'PRIMARY KEY'
    OR tc.constraint_type = 'UNIQUE')
-  AND kc.ordinal_position IS NOT NULL
+  AND kc.ordinal_position IS NOT NULL 
+  GROUP BY  tc.table_schema, tc.table_name, kc.position_in_unique_constraint
 ORDER BY tc.table_schema, tc.table_name, kc.position_in_unique_constraint
-),
+), 
 col AS (
 SELECT c.table_schema, c.table_name, array_to_string(array_agg(c.column_name::text), ', ') as str_col, array_agg(c.column_name::text) as arr_col
 FROM information_schema.columns c
 LEFT JOIN information_schema.key_column_usage kc
-ON kc.table_schema= c.table_schema
+ON kc.table_schema= c.table_schema 
 AND kc.table_name= c.table_name
 AND kc.ordinal_position= c.ordinal_position
 AND kc.column_name = c.column_name
@@ -39,13 +40,13 @@ WHERE kc.constraint_name IS NULL
 GROUP BY c.table_schema, c.table_name
 ORDER BY c.table_schema, c.table_name
 )
-SELECT FORMAT ('SELECT %I, count(%I) as n_duplicate_rows, array_agg(%I) as pks_uks FROM %I.%I GROUP BY %I HAVING count(%I) >1;',
-col.str_col, col.arr_col[1], pks.column_name, col.table_schema, col.table_name, col.str_col, col.arr_col[1]) as sql_statement,
+SELECT FORMAT ('SELECT %I, count(%I) as n_duplicate_rows, array_agg(%I::text) as pks_uks FROM %I.%I GROUP BY %I HAVING count(%I) >1;', 
+col.str_col, col.arr_col[1], pks.column_name, col.table_schema, col.table_name, col.str_col, col.arr_col[1]) as sql_statement, 
 col.table_schema || '.' || col.table_name as schema_table
-FROM pks
-JOIN col
+FROM pks 
+JOIN col 
   ON pks.table_schema= col.table_schema
- AND pks.table_name=col.table_name
+ AND pks.table_name=col.table_name 
 ;""")
 except:
     print "Could not collect data"
@@ -56,18 +57,28 @@ for row in rows:
     print "\n"
     print "Relation name: %s " %  (row[1])
     row=row[0].replace("\"", " ")
-    #print row
+    print row
 
 
     cur2 = conn.cursor()
     try:
         cur2.execute(row)
 
-    except:
-        print "Could not replay queries"
 
-    results = cur2.fetchall()
-    print "Duplicate content | number or duplicates(L) | Values of keys"
-    for result in results:
-        print result
+    except  (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        print "Could not replay queries"
+        conn.rollback()
+    
+    try:
+        results = cur2.fetchall()
+        print "Duplicate content | number or duplicates(L) | Values of keys"
+        for result in results:
+            print result
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+
 
